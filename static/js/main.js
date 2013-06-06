ca6266341283e02763c169950e0d6ee0f8170a0e
Joseph Chow <sortofsleepy@gmail.com>
2013-06-06 16:05:18 -0700

    Updated main leaderhip page a bit. Bios can now be show/revealed, styles cleaned up a bit as well

--- static/js/main.js
+++ static/js/main.js
@@ -93,6 +93,106 @@ var v = new Video({
 ================================*/
 var leaders = $(".leader");
 var leaderlen = leaders.length;
-var contentbox = document.getElementById("single-leader-content");
-var box_height = 500;
 
+
+
+
+for(var i = 0;i<leaderlen;++i){
+	leaders[i].addEventListener("click",function(e){
+		var leader = e.target;
+		
+	
+		var title = document.getElementById("mini-profile-title");
+		var subtitle = document.getElementById("mini-profile-subtitle");
+		var content = document.getElementById("mini-profile-content");
+		
+		//grab aspects from content box
+		var box = $("#content-box");
+		
+	
+		
+		
+		if(!box.hasClass("active")){
+			//set content
+			title.innerHTML = leader.getAttribute("data-name");
+			subtitle.innerHTML = leader.getAttribute("data-title");
+			content.innerHTML = leader.children[0].innerHTML;
+			
+			//set profile image
+			var largeProfile = document.getElementsByClassName("large thumb")[0];
+			largeProfile.id = leader.id;
+		
+	
+			var val = getHeight(box);
+			
+			box.animate({
+				height:val
+			})
+			
+			box.addClass("active");
+		}else{
+			box.animate({
+				height:0
+			},function(){
+				//set content
+				title.innerHTML = leader.getAttribute("data-name");
+				subtitle.innerHTML = leader.getAttribute("data-title");
+				content.innerHTML = leader.children[0].innerHTML;
+				
+				//set profile image
+				var largeProfile = document.getElementsByClassName("large thumb")[0];
+				largeProfile.id = leader.id;
+				
+			var val = getHeight(box);
+			
+			box.animate({
+				height:val
+			})	
+				
+			})
+			
+			box.addClass("active");
+			
+		}
+		
+		
+		//var val = 1.0 * $("#content-box").height(100) + "px";
+		//console.log(val);
+	});
+}
+/*
+    Need to figure out what 100% of the content-box would be
+    because some people might write more content, 
+    thus, throwing off the offsets and height already set
+    
+    Idea : make dummy element and set it to be 
+    what 100% would be.
+    
+    Then animate to whatever that amount is.
+*/
+
+		
+function getHeight(_box){
+	var dummy = document.createElement("div");
+	dummy.id = "content-box";
+	dummy.style.position = "relative";
+	dummy.style.zIndex = -9;
+	dummy.style.bottom = 1000000;
+	dummy.innerHTML = _box[0].innerHTML;
+	dummy.style.height = "100%";
+	$("body").append(dummy);
+	
+	var val = 1.0 * $(dummy).height() + "px";
+	
+	return val;
+
+		
+}
+
+$(".close-button").bind("click",function(){
+	$("#content-box").animate({
+		height:0
+	},function(){
+		$(this).removeClass("active");
+	});
+})
