c527d7361cda197e1fc464c5096e1abd1b7a243f
Joseph Chow <sortofsleepy@gmail.com>
2013-06-10 16:47:39 -0700

    Cleaned up Leadership page a bit. Continued working on contact page.

--- static/js/Mappy.js
+++ static/js/Mappy.js
@@ -11,7 +11,17 @@ var Mappy = function(_params,_options){
 	}
 	
 	
-	//this.map = new google.maps.Map(this.params.mapAnchor, this.options);	
+	//load the map	
+	var parent = this;
+	this.addEvent("load",function(){
+		var options = {
+		  	center:new google.maps.LatLng(33.918589, -118.393719),
+		  	zoom:15,
+		  	mapTypeId:google.maps.MapTypeId.ROADMAP
+	  	}
+		  this.map = new google.maps.Map(document.getElementById(parent.params.mapAnchor), options);	
+		  
+	})
 }
 
 Mappy.prototype = {
@@ -22,6 +32,40 @@ Mappy.prototype = {
 	addEvent:function(_type,_callback){
 		google.maps.event.addDomListener(window,_type,function(){
 			_callback();
+		})
+	},
+	
+	/**
+		NOTE: THESE CAN ONLY BE CALLED AFTER THE LOAD EVENT
+	*/
+	addMarker:function(_address){
+		var address = _address;
+		var parent = this;
+			console.log(this.map);	
+		
+		this.getAddress(_address,function(results){
+			
+			var marker = new google.maps.Marker({
+				map:parent.map,
+				position:results[0].geometry.location
+			});
+			
+		})
+	},
+	
+	
+	getAddress:function(_address,_callback){
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+			"address":_address
+		},function(results,status){
+			
+			if(status == google.maps.GeocoderStatus.OK){
+				_callback(results);
+			}else{
+				console.log("problem getting address coords");
+				return;
+			}
 		})
 	}
 };
