c02dd90ec21550f167903368c12d68068176aed0
Joseph Chow <sortofsleepy@gmail.com>
2013-06-13 14:29:53 -0700

    Did a quick prototype of how featured work might be shown

--- static/js/Work.js
+++ static/js/Work.js
@@ -6,32 +6,49 @@ works.isotope({
 })
 
 
+
 $(".work-in-a-box").each(function(i,obj){
+	obj.setAttribute("data-index", i);
+	
 	obj.addEventListener("click",function(e){
+
+		e.preventDefault();
+		console.log(i);
+		var open = obj.getAttribute("data-open");
 		
-		var elements = [];
-		for(var a = 0;a<workarray.length;++a){
-			elements.push(workarray[a]);
+		if(open){
+			var el = document.getElementsByClassName("work-in-a-box data")[0];
+			works.isotope("remove",$(el));
+			
+			obj.removeAttribute("data-open");
+			works.isotope("reLayout")
+		
+		
+		}else{
+			var elements = [];
+			for(var a = 0;a<workarray.length;++a){
+				elements.push(workarray[a]);
+			}
+			
+			var div = document.createElement("div");
+			div.className = "work-in-a-box mini-work data";
+	
+			//div.className = "mini-work";
+			
+			var loc = parseInt(obj.getAttribute("data-index"));
+			loc += 1;
+		
+			elements.splice(loc ,0,div);
+			works[0].innerHTML = ""
+			
+			works.isotope("insert",$(elements));		
+			
+			
+			works.isotope("reLayout")
+			
+			obj.setAttribute("data-open", true)
 		}
-		
-		var div = document.createElement("div");
-		div.className = "work-in-a-box";
-
-	
-		elements.splice(i,0,div);
-
-
-		works[0].innerHTML = ""
-	
-		for(var b = 0;b<elements.length;++b){
-			works.append(elements[b]);
-		
-		works.isotope("reLayout",function(){
-			console.log("re-doing");
-		});
-		}		
-		
-		
 	})
 })
+
 
