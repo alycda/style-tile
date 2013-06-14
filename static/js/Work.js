f2d0a83fc314d50469e5cb3078f5799a8e1b3050
Joseph Chow <sortofsleepy@gmail.com>
2013-06-13 17:25:25 -0700

    On work example page, little arrow now appears when you click on a item

--- static/js/Work.js
+++ static/js/Work.js
@@ -5,26 +5,35 @@ works.isotope({
 	itemSelector:".work-in-a-box",
 })
 
-
+//helps to make sure only one piece can be seen at a time.
+var contentopen = false;
 
 $(".work-in-a-box").each(function(i,obj){
+
+	//set the index as a attribute as it'll play into things later
 	obj.setAttribute("data-index", i);
 	
 	obj.addEventListener("click",function(e){
+		
+		
 
-		e.preventDefault();
-		console.log(i);
+		//see if the object we clicked on is "open"
 		var open = obj.getAttribute("data-open");
 		
-		if(open){
+		//if it's open and contentopen is not false, init closing process.
+		if((open)&&(contentopen)){
+			//get the elment
 			var el = document.getElementsByClassName("work-in-a-box data")[0];
 			works.isotope("remove",$(el));
-			
 			obj.removeAttribute("data-open");
 			works.isotope("reLayout")
 		
-		
-		}else{
+			//recent contentopen
+			contentopen = false;
+			
+		//otherwise start oepn process
+		}else if(contentopen == false){
+			contentopen = true;
 			var elements = [];
 			for(var a = 0;a<workarray.length;++a){
 				elements.push(workarray[a]);
@@ -32,20 +41,26 @@ $(".work-in-a-box").each(function(i,obj){
 			
 			var div = document.createElement("div");
 			div.className = "work-in-a-box mini-work data";
-	
-			//div.className = "mini-work";
+			
+			//get content
+			var content = obj.parentNode.children[0].children[1];
+			
+			//add a arrow indicating which item was clicked "open"
+			var arrow = content.getElementsByClassName("arrow")[0];
+			arrow.style.marginLeft = e.x - works.offset().left + "px";
+			div.innerHTML = content.innerHTML;
+			
+			
 			
 			var loc = parseInt(obj.getAttribute("data-index"));
 			loc += 1;
+			
 		
-			elements.splice(loc ,0,div);
-			works[0].innerHTML = ""
+			elements.splice(loc,0,div);
+			works[0].innerHTML = "";
 			
-			works.isotope("insert",$(elements));		
-			
-			
+			works.isotope("insert",$(elements));				
 			works.isotope("reLayout")
-			
 			obj.setAttribute("data-open", true)
 		}
 	})
